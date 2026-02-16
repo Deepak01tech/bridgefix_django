6eff953cecc99e2da79d9538c1bf2c38bbe601ca
Deepak <dp55954@gmail.com>
2026-02-16 18:42:55 +0530

    template

---
+++ my_first/firstapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from firstapp import views
+from . import views
+
+urlpatterns = [
+    path('',views.member, name='members'),
+    path('details/<int:id>/',views.details, name='details'),
+]
